35febde6b2f1164b5856c27fd70045c9908f9951
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2024-06-28 09:49:04 -0400

    Create addNoPlus.go

---
+++ Go/addNoPlus.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d === 4\n", add(2, 2))
+	fmt.Printf("%d === 0\n", add(-120, 120))
+	fmt.Printf("%d === 95\n", add(97, -2))
+	fmt.Printf("%d === 330\n", add(120, 210))
+
+}
+
+func add(x int, y int) int {
+	var keep = (x & y) << 1
+	var res = x ^ y
+
+	// If bitwise & is 0, then there
+	// is not going to be any carry.
+	// Hence result of XOR is addition.
+	if keep == 0 {
+		return res
+	}
+	return add(keep, res)
+}
